244897aadbf9ee8e25c2808031f0a825328bd725
Jordan Lim <80717189+jordlim@users.noreply.github.com>
2024-10-29 18:36:31 +0000

    Create basic layout with books, add simple styling

---
+++ library.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    if (read == True)
+        return this.title+" by "+this.author+", "+pages+" pages, " +"read"
+    else
+        return this.title+" by "+this.author+", "+pages+" pages, " +"not read"
+  }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
